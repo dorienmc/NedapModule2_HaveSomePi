d24339f55ff36ed4698ea00ba013eb04dc03b51d
Dorien Meijer Cluwen <dorien.meijercluwen@nvc3141.nedap.local>
2017-04-12 22:15:51 +0200

    Setup ARP protocol

---
+++ src/main/java/com/nedap/university/fileTranser/ARQProtocol/NaiveProtocol.java
@@ -0,0 +1,53 @@
+package com.nedap.university.fileTranser.ARQProtocol;
+
+import com.nedap.university.fileTranser.Receiver;
+import com.nedap.university.fileTranser.Sender;
+import com.nedap.university.fileTranser.UDPPacket;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Very naive protocol that just send data and does send acks.
+ * Doesnt change the sequence number either. Also sends all data at once.
+ * Created by dorien.meijercluwen on 09/04/2017.
+ */
+public class NaiveProtocol extends Protocol{
+  public static final int MAX_RESEND = 10;
+  private int resendCount;
+
+  public NaiveProtocol(Sender sender, Receiver receiver) {
+    super(sender, receiver);
+  }
+
+  /* Send given data (in parts) and wait for response */
+  public void send(byte[] data) throws IOException {
+    resendCount = 0;
+
+    //Send data in 1 go
+    UDPPacket packet = new UDPPacket(sender.getSourcePort(), sender.getDestPort(), 0, 0);
+    packet.setData(data);
+
+    while(resendCount < MAX_RESEND) {
+      sender.addPacketToBuffer(packet);
+      sender.unBlockSender();
+
+      //Wait for response
+      try {
+        UDPPacket response = super.receivePacket();
+
+        //Save data to 'dataReceived'
+        super.addReceivedData(response.getData());
+        break;
+
+      } catch (TimeoutException e) {
+        System.out.println("TIME OUT " + resendCount);
+      }
+
+      resendCount++;
+    }
+
+    if(resendCount >= MAX_RESEND) {
+      throw new IOException("Resend limit of " + MAX_RESEND + " exceeded.");
+    }
+  }
+}
--- src/main/java/com/nedap/university/fileTranser/ARQProtocol/Protocol.java
+++ src/main/java/com/nedap/university/fileTranser/ARQProtocol/Protocol.java
@@ -1,9 +1,82 @@
 package com.nedap.university.fileTranser.ARQProtocol;
 
+import com.nedap.university.fileTranser.Receiver;
+import com.nedap.university.fileTranser.ReliableUdpChannel;
+import com.nedap.university.fileTranser.Sender;
+import com.nedap.university.fileTranser.UDPPacket;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
+
 /**
- * Interface for an ARQ protocol
+ * Abstract class for an ARQ protocol
  * Created by dorien.meijercluwen on 09/04/2017.
  */
-public interface Protocol {
+public abstract class Protocol {
+  public static final int MAX_BUFFER = 1500;
+  byte[] dataToSend;
+  byte[] dataReceived;
+  Sender sender;
+  Receiver receiver;
+  public static final int TIMEOUT = 7000;
 
+  int seqNumber; //Sequence number of next packet that is to be send.
+  int ackNumber; //Sequence number of next expected packet from receiver.
+
+  public Protocol(Sender sender, Receiver receiver) {
+    this.sender = sender;
+    this.receiver = receiver;
+    this.dataToSend = new byte[0];
+    this.dataReceived = new byte[0];
+  }
+
+  //TODO split file into multiple UDP packets
+
+  /* Send given data (in parts) and wait for acks if requested */
+  public abstract void send(byte[] data) throws IOException;
+
+  /* Send packet over socket */
+  protected void sendPacket(UDPPacket packet) {
+    sender.addPacketToBuffer(packet);
+  }
+
+  /* Wait for next packet over socket */
+  protected UDPPacket receivePacket() throws IOException,TimeoutException {
+    UDPPacket response = receiver.retrievePacket();
+    int time = 0;
+
+    while(response == null) {
+      //Wait
+      try {
+        Thread.sleep(10);
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+
+      time += 10;
+      if(time > TIMEOUT) {
+        throw new TimeoutException("Exceeded time out, resend");
+      }
+
+      response = receiver.retrievePacket();
+    }
+    return response;
+  }
+
+  /* Add data to received data */
+  protected void addReceivedData(byte[] data) {
+    int oldLength = dataReceived.length;
+    int dataLength = data.length;
+    dataReceived = Arrays.copyOf(dataReceived, oldLength + dataLength);
+    System.arraycopy(data,0,dataReceived,oldLength,dataLength);
+  }
+
+  /* Return received data */
+  public byte[] receive() {
+    return dataReceived;
+  }
 }
+

7f1de093640bcc650d76891a02eeb34816898e46
Dorien Meijer Cluwen <dorien.meijercluwen@nvc3141.nedap.local>
2017-04-12 22:16:31 +0200

    Sender and Receiver classes for ARP protocol.

---
+++ src/main/java/com/nedap/university/fileTranser/Receiver.java
@@ -0,0 +1,89 @@
+package com.nedap.university.fileTranser;
+
+import com.nedap.university.fileTranser.ARQProtocol.Protocol;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+/**
+ * Created by dorien.meijercluwen on 12/04/2017.
+ */
+public class Receiver extends Thread {
+  DatagramSocket socket;
+  int portIn;
+  int portOut;
+  InetAddress address;
+  private ConcurrentLinkedDeque<UDPPacket> receiveBuffer; //Packets that have arrived
+  boolean isBlocked;
+  boolean stop;
+  int currentAckNumber; //Sequence number of next expected packet from receiver.
+
+  public Receiver(DatagramSocket socket, InetAddress address, int portIn, int portOut) {
+    this.socket = socket;
+    this.receiveBuffer = new ConcurrentLinkedDeque<>();
+    this.isBlocked = false;
+    this.stop = false;
+    this.currentAckNumber = 0;
+    this.address = address;
+    this.portIn = portIn;
+    this.portOut = portOut;
+  }
+
+  public int getCurrentAckNumber() {
+    return currentAckNumber;
+  }
+
+  public int getPortIn() {
+    return portIn;
+  }
+
+  /* Retrieve next packet from the receiveBuffer, null there are none.
+    * Note: also removes this packet from the buffer! */
+  public UDPPacket retrievePacket() {
+    return receiveBuffer.pollFirst();
+  }
+
+  public void blockReceiver() { //TODO needed?
+    isBlocked = true;
+  }
+
+  public void unBlockReceiver() {
+    isBlocked = false;
+  }
+
+  @Override
+  public void run() {
+
+    while(!stop) {
+      //Receive packets from socket when not blocked
+      if(!isBlocked) {
+        DatagramPacket response = new DatagramPacket(new byte[Protocol.MAX_BUFFER],Protocol.MAX_BUFFER);
+        try {
+          socket.receive(response);
+        } catch (IOException e) {
+          System.out.println(String.format("Error in receiver: %s",e.getMessage()));
+        }
+
+        UDPPacket packet = new UDPPacket(response);
+        System.out.println("Received new packet " + packet);
+        currentAckNumber = packet.getAckNumber();
+        receiveBuffer.add(packet);
+
+        //TODO handle acks?
+      }
+
+      //Wait
+      try {
+        Thread.sleep(10);
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+    }
+  }
+
+  public void shutdown() {
+    stop = true;
+  }
+}
---
+++ src/main/java/com/nedap/university/fileTranser/Sender.java
@@ -0,0 +1,117 @@
+package com.nedap.university.fileTranser;
+
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+/**
+ * Created by dorien.meijercluwen on 12/04/2017.
+ */
+public class Sender extends Thread {
+  DatagramSocket socket;
+  int destPort;
+  int sourcePort;
+  InetAddress address;
+  private ConcurrentLinkedDeque<UDPPacket> sendBuffer; //Packets that still need to be send
+  private ConcurrentHashMap<Integer,UDPPacket> unAckedPackets; //Packets that are still unacked.
+  boolean isBlocked;
+  boolean stop;
+  int currentSeqNumber; //Sequence number of last packet that was send.
+
+  /*
+  * Create new sender, starts blocked
+   */
+  public Sender(DatagramSocket socket, InetAddress address, int sourcePort, int destPort) {
+    this.socket = socket;
+    this.sendBuffer = new ConcurrentLinkedDeque<>();
+    this.unAckedPackets = new ConcurrentHashMap<>();
+    this.isBlocked = true;
+    this.stop = false;
+    this.address = address;
+    this.destPort = destPort;
+    this.sourcePort = sourcePort;
+    this.currentSeqNumber = 0;
+    System.out.println(socket.getLocalPort());
+  }
+
+  public int getCurrentSeqNumber() {
+    return currentSeqNumber;
+  }
+
+  public int getSourcePort() {
+    return sourcePort;
+  }
+
+  public int getDestPort() {
+    return destPort;
+  }
+
+  public void addPacketToBuffer(UDPPacket packet) { //TODO add sequence number here?
+    sendBuffer.add(packet);
+  }
+
+  public void blockSender() {
+    isBlocked = true;
+  }
+
+  public void unBlockSender() {
+    isBlocked = false;
+  }
+
+  @Override
+  public void run() {
+
+    while(!stop) {
+      //Send packets when there are packets in the queue and the sender is not blocked
+      if(!isBlocked) {
+        //Send packet from buffer
+        try {
+          sendPacket(sendBuffer.pollFirst());
+        } catch (IOException e) {
+          System.out.println("WARNING could not send packet with seq. number" + currentSeqNumber);
+          Thread.currentThread().interrupt();
+        }
+      }
+
+      //Wait
+      try {
+        Thread.sleep(10);
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+    }
+  }
+
+  private void sendPacket(UDPPacket packet) throws IOException {
+    if (packet != null) {
+      System.out.println("Try to send packet: " + packet);
+      System.out.println("To " + address + ":" + packet.getDestPort());
+      System.out.println(packet.getSourcePort());
+      //Update current seq number
+      currentSeqNumber = packet.getSequenceNumber();
+
+      //Add to unacked (and (re)set timeout?) //TODO reset timeout?
+      unAckedPackets.put(new Integer(packet.getSequenceNumber()), packet);
+
+      //Try to send
+      socket.send(packet.toDatagram(address));
+    }
+  }
+
+  public void forceSend(UDPPacket packet) {
+    try {
+      sendPacket(packet);
+    } catch (IOException e) {
+      System.out.println("Warning, could not force send packet " + packet);
+    }
+  }
+
+  public void shutdown() {
+    stop = true;
+  }
+
+
+
+}

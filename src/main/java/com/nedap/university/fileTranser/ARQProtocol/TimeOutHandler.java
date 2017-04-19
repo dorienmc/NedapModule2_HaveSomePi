608c4789639a1c42760b4c75fb63cdb07543df48
Dorien Meijer Cluwen <dorien.meijercluwen@nvc3141.nedap.local>
2017-04-19 16:45:04 +0200

    TimeOutHandler, remove unnessecary code, remove timers safely while looping over them.

--- src/main/java/com/nedap/university/fileTranser/ARQProtocol/TimeOutHandler.java
+++ src/main/java/com/nedap/university/fileTranser/ARQProtocol/TimeOutHandler.java
@@ -2,13 +2,10 @@ package com.nedap.university.fileTranser.ARQProtocol;
 
 import com.nedap.university.Utils;
 import com.nedap.university.fileTranser.UDPPacket;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.TimeoutException;
 
 /**
  * Created by dorien.meijercluwen on 15/04/2017.
@@ -41,10 +38,8 @@ public class TimeOutHandler extends Thread {
     this.stop = false;
   }
 
-  //TODO overwrite old entry or keep older?
+  /** Add new timer for the given packet, overwrites old timer if there is one **/
   public void addTimer(UDPPacket packet) {
-    System.out.println("Create timer for packet with seq " + packet.getSequenceNumber()
-        + " of request " + packet.getRequestId());
     timers.put(packet, new Timer(timeOut));
   }
 
@@ -52,17 +47,11 @@ public class TimeOutHandler extends Thread {
   public boolean stopTimer(int sequenceNumber) {
     for(UDPPacket packet: timers.keySet()) {
       if(packet.getSequenceNumber() == sequenceNumber) {
-        System.out.println("Stopping timer of packet with seq: " + sequenceNumber);
         timers.remove(packet);
         return true;
       }
     }
     return false;
-  }
-
-  /* Stop timer of given packet */
-  private boolean stopTimer(UDPPacket packet) {
-    return (timers.remove(packet) != null);
   }
 
   public synchronized void stopHandler() {
@@ -76,26 +65,38 @@ public class TimeOutHandler extends Thread {
   @Override
   public void run() {
     while(!stop) {
-      for(Entry<UDPPacket,Timer> entry: timers.entrySet()) {
+      Iterator<Entry<UDPPacket,Timer>> iterator = timers.entrySet().iterator();
+      while(iterator.hasNext()) {
+        Map.Entry<UDPPacket,Timer> entry = iterator.next();
+        UDPPacket packet = entry.getKey();
+        Timer timer = entry.getValue();
+
         //Check if timer has elapsed.
-        if(entry.getValue().hasElapsed()) {
-          //Add to resend buffer
-          UDPPacket packet = entry.getKey();
-          System.out.println("Time out of packet with seqNumber " + packet.getSequenceNumber() + " has elapsed.");
-          stopTimer(packet); //TODO check if removing from map is possible during iterating over it!
+        if(timer.hasElapsed()) {
+          //Remove entry from map
+          iterator.remove();
+
+          //Add packet to resend buffer
           protocol.addPacketToResendBuffer(packet);
         }
       }
+
 
       Utils.sleep(10);
     }
 
     //Stop all timers
-    for(Entry<UDPPacket,Timer> entry: timers.entrySet()) {
+    Iterator<Entry<UDPPacket,Timer>> iterator = timers.entrySet().iterator();
+    while(iterator.hasNext()) {
+      Map.Entry<UDPPacket,Timer> entry = iterator.next();
       UDPPacket packet = entry.getKey();
-      stopTimer(packet);
-      protocol.addPacketToReceiverBuffer(packet);
 
+      //Remove entry from map
+      iterator.remove();
+
+      //Add packet to resend buffer
+      protocol.addPacketToResendBuffer(packet);
+    }
   }
 
 }
